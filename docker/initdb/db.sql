d10dd3ef5afbf63b0c1917f72b6bcf66b460bcb6
albertopformoso <albertopformoso@gmail.com>
2022-05-03 21:54:23 -0600

    docker compose db configuration

---
+++ docker/initdb/db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE golang;
+USE golang;
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2006 DEFAULT CHARSET=latin1;
+
+LOCK TABLES `employees` WRITE;
+
+INSERT INTO `employees` VALUES
+    (2000, 'Steve', 'steve@work.com'),
+    (2001, 'Arian', 'arian@work.com');
+
+UNLOCK TABLES;
